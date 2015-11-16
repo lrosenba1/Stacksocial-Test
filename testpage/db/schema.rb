1017cebbf2ff3405cc53c142d3d8a70169315f9c
littlemouseyuki <lisa@Lisas-MBP-2.home>
2015-11-16 12:41:05 -0800

    changed to postgresql from sqlite3.

--- testpage/db/schema.rb
+++ testpage/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151116003948) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "newusers", force: true do |t|
     t.string   "username"
     t.string   "password_digest"
