cbd0562bc4802b1cf8b8e31574d35cee10596fe0
littlemouseyuki <lisa@Lisas-MBP-2.home>
2015-11-16 13:14:16 -0800

    serving static assets.

--- testpage/config/application.rb
+++ testpage/config/application.rb
@@ -20,5 +20,6 @@ module Testpage
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.autoload_paths << Rails.root.join('lib')
+    config.serve_static_assets = true
   end
 end
